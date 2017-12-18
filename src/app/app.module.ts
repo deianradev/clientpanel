2400273b73497ff9bfd9d8b2bc018b13386e97a5
dmr <deian.m.radev@gmail.com>
2017-12-17 22:14:33 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,67 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+import { ClientService } from './services/client.service';
+
+//create routes
+const appRoutes: Routes = [
+  {path:'', component: DashboardComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component:LoginComponent},
+  {path:'add-client', component:AddClientComponent},
+  {path:'client/:id', component:ClientDetailsComponent},
+  {path:'edit-client/:id', component:EditClientComponent}  
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    FlashMessagesModule,
+    FormsModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase, 'clientpanel'),
+    AngularFireAuthModule
+  ],
+  providers: [
+    AngularFireDatabase,
+    AngularFireDatabaseModule,
+    ClientService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
